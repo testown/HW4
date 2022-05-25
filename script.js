3695a95acac78047dac00376236ca071b97f5ae2
yuriy <59261753+testown@users.noreply.github.com>
2022-05-25 21:54:08 +0300

    Home Work 4, NaN to String

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿const number1 = Number(prompt('Enter first number:'));
+const operation = prompt('Enter operation:');
+const number2 = Number(prompt('Enter second number:'));
+
+if (String(number1) !== 'NaN' && String(number2) !== 'NaN') {
+    if (operation === '+') {
+        alert('Ваш результат: ' + (number1 + number2));
+    } else if (operation === '-') {
+        alert('Ваш результат: ' + (number1 - number2));
+    } else if (operation === '*') {
+        alert('Ваш результат: ' + (number1 * number2));
+    } else if (operation === '/') {
+        alert('Ваш результат: ' + (number1 / number2));
+    } else {
+        alert('Такой операции не существует');
+    }
+} else {
+    alert('Number 1 or number 2 is not a number!')
+}
